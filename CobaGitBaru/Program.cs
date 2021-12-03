21fcf64ee4d8f227ad00b9ab5807b47e8cf72050
cete995 <95453164+cete995@users.noreply.github.com>
2021-12-03 09:38:29 +0700

    Melakukan output teks lagu aku seorang kapiten

--- CobaGitBaru/Program.cs
+++ CobaGitBaru/Program.cs
@@ -7,6 +7,7 @@ namespace CobaGitBaru
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Aku adalah seorang kapiten mempunyai pedang panjang");
         }
     }
 }
